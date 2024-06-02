c820441b0854885a94e7d298b64d7f887f8e7624
habibb97 <45489927+habibb97@users.noreply.github.com>
2024-06-02 13:57:51 +0700

    tes2

---
+++ main.js
@@ -0,0 +1,9 @@
+let hari = new Date().getDay()
+function myFunction() {
+    document.getElementById("paragraph").innerHTML = "Hello Dolly!";
+    document.getElementById("teks1").innerHTML = "How are you?";
+}
+
+function ubah(){
+    document.getElementById("paragraph").innerHTML = "tes"
+}
